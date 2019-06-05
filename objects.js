760b90df28cf8d5eed8ce50212ff4a728eddaf8c
Richard Lee <richlee81192@gmail.com>
2019-06-05 01:21:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { Drake: "kiki" }
 function updatePlaylist(playlist, artistname,songTitle) {
   playlist[artistname] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistname) {
+  delete playlist.artistname
+}
